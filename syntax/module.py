1fe5270ded13ac8129fecb18f47758774389ee4d
nsunwoo <sunwoo.stats@gmail.com>
2022-08-09 00:07:38 +0900

    문법 - 모듈

---
+++ syntax/module.py
@@ -0,0 +1,11 @@
+# import math_module
+
+# print(math_module.average(1, 2, 3))
+# print(math_module.plus(1, 2))
+# print(math_module.pi)
+
+from math_module import average, plus, pi
+
+print(average(1, 2, 3))
+print(plus(1, 2))
+print(pi)
